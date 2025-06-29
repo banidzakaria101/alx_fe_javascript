d906662fd68546f4c84ad4fad19061d7ac3e0062
zakariabanid101 <banidzakaria2@gmail.com>
2025-06-29 23:32:53 +0100

    add function to export json file

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -184,6 +184,8 @@ function startSync() {
   setInterval(syncFromServer, SYNC_INTERVAL);
 }
 
+
+
 // —————————— Initialization ——————————
 
 document.getElementById("syncButton")?.addEventListener("click", syncQuotes);
@@ -199,3 +201,16 @@ if (document.getElementById("categoryFilter")) {
   document.getElementById("categoryFilter").onchange = filterQuotes;
 }
 startSync();
+
+function exportToJsonFile() {
+  const dataStr = JSON.stringify(quotes, null, 2);
+  const blob = new Blob([dataStr], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "quotes.json";
+  a.click();
+
+  URL.revokeObjectURL(url); 
+}
